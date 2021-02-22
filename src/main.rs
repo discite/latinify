24df5ff61d4550587f84ee198aa59fe8b3d46b6e
discite-mac <4948877+discite@users.noreply.github.com>
2021-02-22 14:56:01 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("OK")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .service(health)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("0.0.0.0:8000")?
+    .run()
+    .await
+}
